43777bf13ce84d332d301899931f38693a72ebd6
kxxt <rsworktech@outlook.com>
2020-10-29 16:17:03 +0800

    Initialize environment.

---
+++ init.sh
@@ -0,0 +1,6 @@
+pip3 install selenium && 
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip &&
+unzip chromedriver_linux64.zip &&
+sudo mv chromedriver /usr/bin/chromedriver &&
+sudo chown root:root /usr/bin/chromedriver &&
+sudo chmod +x /usr/bin/chromedriver
